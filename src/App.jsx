0b39aa16880e8b7dc47a07955fe00d4fa36ae9d0
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-04-01 09:38:42 +0530

    Made Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 
 export default function App() {
   return (
-    <div className="background h-screen pt-40">
+    <div className="background h-screen md:pt-40 pt-4">
       <Home />
     </div>
   )
